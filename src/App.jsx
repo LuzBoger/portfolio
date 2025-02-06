52aac3bffdd18ec06128d4a051a9efa996bb13dd
LuzBoger <arthurbrouard2003@gmail.com>
2025-02-06 22:49:34 +0100

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+    return (
+        <>
+            <div className="min-h-screen flex flex-col bg-primary-50">
+                <div className="">
+                    testFDQS
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
