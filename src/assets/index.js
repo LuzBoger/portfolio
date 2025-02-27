c3103268d48aaee9c120bd8c1476f3b791151ddb
LuzBoger <arthurbrouard2003@gmail.com>
2025-02-27 22:47:42 +0100

    feature: add asset and index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,12 @@
 import ReactIcon from './react.svg'
 import SpringBootIcon from './springboot.svg'
+import ReduxIcon from './redux.svg'
+import StoryBookIcon from './storybook.svg'
+import TailwindIcon from './tailwind.svg'
+import PrettierIcon from './prettier.svg'
+import JavascriptIcon from './js.svg'
+import JqueryIcon from './jquery.svg'
+import HtmlIcon from './html.svg'
+import CssIcon from './css.svg'
 
-export { ReactIcon, SpringBootIcon }
+export { ReactIcon, SpringBootIcon, ReduxIcon, StoryBookIcon, TailwindIcon, PrettierIcon, JavascriptIcon, JqueryIcon, HtmlIcon, CssIcon }
